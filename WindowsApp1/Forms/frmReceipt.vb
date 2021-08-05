b52401c586d33a702c05aeb01762234b07f936e7
chaluka hashan <chaluka hashan@DESKTOP-V94BHU7>
2021-08-05 22:32:35 +0530

    tkt

--- WindowsApp1/Forms/frmReceipt.vb
+++ WindowsApp1/Forms/frmReceipt.vb
@@ -1,3 +1,13 @@
-﻿Public Class frmReceipt
+﻿
+Imports Microsoft.Reporting.WinForms
+
+Public Class frmReceipt
+    Private Sub frmReceipt_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReportViewer1.LocalReport.ReportPath = "c:\users\chaluka hashan\desktop\p3\WindowsApp1\Reports\DepartureTicker.rdlc"
+
+        Dim r(o) As ReportParameter
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
 
 End Class
--- WindowsApp1/My Project/Settings.Designer.vb
+++ WindowsApp1/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.1.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -51,6 +51,17 @@ Namespace My
                 End If
 #End If
                 Return defaultInstance
+            End Get
+        End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=Tru"& _ 
+            "e;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False")>  _
+        Public ReadOnly Property masterConnectionString() As String
+            Get
+                Return CType(Me("masterConnectionString"),String)
             End Get
         End Property
     End Class
