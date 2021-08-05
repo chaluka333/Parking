5142b965dc86ca58f998ab5559d118756307e4de
chaluka hashan <chaluka hashan@DESKTOP-V94BHU7>
2021-08-05 22:45:37 +0530

    Revert "tkt"
    This reverts commit b52401c586d33a702c05aeb01762234b07f936e7.

--- WindowsApp1/Forms/frmReceipt.Designer.vb
+++ WindowsApp1/Forms/frmReceipt.Designer.vb
@@ -22,43 +22,16 @@ Partial Class frmReceipt
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'ReportViewer1
-        '
-        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "ParkingLotManagementSystem.DepartureTicker.rdlc"
-        Me.ReportViewer1.Location = New System.Drawing.Point(40, 159)
-        Me.ReportViewer1.Name = "ReportViewer1"
-        Me.ReportViewer1.ServerReport.BearerToken = Nothing
-        Me.ReportViewer1.Size = New System.Drawing.Size(712, 226)
-        Me.ReportViewer1.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Old English Text MT", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(219, 41)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(354, 57)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "rdlc for depature"
         '
         'frmReceipt
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "frmReceipt"
         Me.Text = "frmReceipt"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
-    Friend WithEvents Label1 As Label
 End Class
--- WindowsApp1/Forms/frmReceipt.vb
+++ WindowsApp1/Forms/frmReceipt.vb
@@ -1,13 +1,3 @@
-﻿
-Imports Microsoft.Reporting.WinForms
-
-Public Class frmReceipt
-    Private Sub frmReceipt_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ReportViewer1.LocalReport.ReportPath = "c:\users\chaluka hashan\desktop\p3\WindowsApp1\Reports\DepartureTicker.rdlc"
-
-        Dim r(o) As ReportParameter
-
-        Me.ReportViewer1.RefreshReport()
-    End Sub
+﻿Public Class frmReceipt
 
 End Class
